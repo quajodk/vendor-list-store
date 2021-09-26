81e96ff0916857c4c6d802a2b2c8ce4f2b4e658e
Quajo Duke <quajoduke305@gmail.com>
2021-09-26 15:21:16 +0000

    push after adding secrets

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,24 +1,19 @@
-import * as Route from 'constants/routes';
-import logo from 'images/logo-full.png';
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import * as Route from "constants/routes";
+import logo from "images/logo-full.png";
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Footer = () => {
   const { pathname } = useLocation();
 
-  const visibleOnlyPath = [
-    Route.HOME,
-    Route.SHOP
-  ];
+  const visibleOnlyPath = [Route.HOME, Route.SHOP];
 
   return !visibleOnlyPath.includes(pathname) ? null : (
     <footer className="footer">
       <div className="footer-col-1">
         <strong>
           <span>
-            Developed by
-            {' '}
-            <a href="https://github.com/jgudo">JULIUS GUEVARRA</a>
+            Developed by <a href="#">Quajo Duke</a>
           </span>
         </strong>
       </div>
@@ -29,14 +24,7 @@ const Footer = () => {
           {new Date().getFullYear()}
         </h5>
       </div>
-      <div className="footer-col-3">
-        <strong>
-          <span>
-            Fork this project &nbsp;
-            <a href="https://github.com/jgudo/ecommerce-react">HERE</a>
-          </span>
-        </strong>
-      </div>
+      <div className="footer-col-3"></div>
     </footer>
   );
 };
