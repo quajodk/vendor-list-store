f961399200b837298b9107c2aeee72fb998bef98
Quajo Duke <quajoduke305@gmail.com>
2021-10-05 00:51:03 +0000

    changes to the app

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,5 +1,5 @@
 import * as Route from "constants/routes";
-import logo from "images/logo-full.png";
+import logo from "images/logo_size.jpg";
 import React from "react";
 import { useLocation } from "react-router-dom";
 
@@ -13,18 +13,32 @@ const Footer = () => {
       <div className="footer-col-1">
         <strong>
           <span>
-            Developed by <a href="#">Quajo Duke</a>
+            Developed with <span>❤️</span> by <a href="#">Quajo Duke</a>
           </span>
         </strong>
       </div>
-      <div className="footer-col-2">
-        <img alt="Footer logo" className="footer-logo" src={logo} />
-        <h5>
-          &copy;&nbsp;
-          {new Date().getFullYear()}
-        </h5>
+      <div className="footer-col-2"></div>
+      <div className="footer-col-3">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "end",
+          }}
+        >
+          <img
+            alt="Footer logo"
+            className="footer-logo"
+            src={logo}
+            style={{ height: 50, padding: 10 }}
+          />
+          <span>
+            &copy;&nbsp;
+            {new Date().getFullYear()}
+          </span>
+        </div>
       </div>
-      <div className="footer-col-3"></div>
     </footer>
   );
 };
